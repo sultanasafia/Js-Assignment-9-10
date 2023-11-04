8a9b9c54ab937c0fd4c1c07fde77bdae201f899f
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-04 10:48:52 +0500

    js assignment 9-10

---
+++ app.js
@@ -0,0 +1,109 @@
+// Q1==========================
+// let user = prompt('Enter your city name')
+// if(user === "karachi"){
+//     alert("Welcome to city of lights "+ user)
+// }
+// Q2===========================
+// let user = prompt('Enter your gender')
+// if(user === "male"){
+//     alert(' Good Morning Sir')
+// }else if (user === "female"){
+//     alert(' Good Morning  Ma’am')
+
+// }else{
+//     alert('Hello')
+// }
+// Q3======================
+// let user = prompt('Enter traffic signal color')
+// if(user === "red"){
+    //     alert('Must Stop')
+    // }else if(user === "yellow"){
+        //     alert('Ready to move')
+        // }else if(user === "green"){
+            //     alert('Move now')
+            
+            // }
+// Q4======================
+// let fuel = prompt("enter your fuel")
+// if(fuel === '0.25litres'){
+//     alert('Please refill the fuel in your car')
+// }
+// Q5=================
+// let a = 4
+// let b = 82
+// if(++a === 5){
+    //     alert("given condition for variable a is true")
+    // }if(b++ === 83){
+        //     alert("given condition for variable b is true")
+        
+        // }
+// let d = 12
+// if (++d === 13){
+//     alert("condition is true")
+// }else{
+//     alert("wrong")
+// }
+// Q6=============
+// let userP= prompt("ente you pecentage")
+// let percentage ;
+// if(userP >= 80){
+//     alert("A-one\n Excellent")
+// }else if (userP >= 70){
+//     alert("A\n Good")
+
+// }else if (userP >= 60){
+//     alert("B\n You need to improve")
+
+// }else if(userP <60){
+//     alert('Fail')
+// }
+
+
+// Q7====================
+// let secNum = 6;
+// let user = +prompt("Enter your number")
+// if(user === 6){
+//     alert('yes this is '+ secNum)
+// }else if (user === 5){
+//     alert("nearest number")
+// }
+// Q8==============
+// let user = prompt('The given number is divisible by 3')
+// if(user % 3 === 0){
+//     alert('yes it divisible by 3')
+// }
+// Q9===========
+// let user  =prompt('Enter number')
+// if(user % 2 === 0){
+//     alert('this is even number')
+// }else{
+//     alert('this is odd number')
+// }
+// Q10==============
+// let temprature = +prompt('enter temprature number');
+// if(temprature > 40){
+//     alert('It is too hot outside')
+// }else if (temprature > 30 ){
+//     alert('The Weather today is Normal.')
+// }else if (temprature> 20){
+//     alert('Today’s Weather is cool.')
+
+// }else if (temprature > 10 ){
+//     alert('OMG! Today’s weather is so Cool.”')
+// }
+// Q11==============
+// let num1 = +prompt("Enter number1 ")
+// let num2 = +prompt("Enter number2 ")
+// let oper = prompt("+, -, *, /, % ")
+// if(oper === "+"){
+//     alert(num1+num2)
+// }else if(oper === "-"){
+//     alert(num1-num2)
+// }else if (oper === "*"){
+//     alert(num1*num2)
+// }else if (oper === "/"){
+//     alert(num1/num2)
+// }else if(oper === "%"){
+//     alert(num1%num2)
+// }
+// task completed=================
